b13831fc2d5ed2d01816f391025be2784efbd49b
[Angel Soto] <[angel.14.98@hotmail.com]>
2020-08-20 12:15:26 -0500

    LocalStorage creado

--- src/classes/todo.list.class.js
+++ src/classes/todo.list.class.js
@@ -1,22 +1,25 @@
 export class TodoList {
 
     constructor(){
-        this.todos = [];
+        this.cargarLocalStorage();
     }
 
     nuevoTodo(todo){
         this.todos.push(todo);
+        this.guardarLocalStorage();
     }
 
     eliminarTodo(id){
         this.todos = this.todos.filter(todo => todo.id != id);
         //filtra todos los todo que no tengan el mismo id que el enviado
+        this.guardarLocalStorage();
     }
 
     marcarCompletado(id){
         for (const todo of this.todos){
             if(todo.id == id){
                 todo.coompletado = !todo.coompletado;
+                this.guardarLocalStorage();
                 break;
             }
         }
@@ -25,5 +28,21 @@ export class TodoList {
     eliminarCompletados(){
         this.todos = this.todos.filter(todo => !todo.coompletado);
         //Regresa todos los que no estan completados
+        this.guardarLocalStorage();
+    }
+
+    guardarLocalStorage(){
+        localStorage.setItem('todo',JSON.stringify(this.todos));
+    }
+
+    cargarLocalStorage(){
+        // if(localStorage.getItem('todo')){
+        //     this.todos = JSON.parse(localStorage.getItem('todo'));
+        // }else{
+        //     this.todos = [];
+        // }
+        this.todos = (localStorage.getItem('todo')) ? 
+            JSON.parse(localStorage.getItem('todo')): 
+            this.todos = [];
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,7 @@ import {Todo, TodoList} from './classes' //index.js por defecto
 import { crearTodoHtml } from './js/componentes';
 // import { Todo } from './classes/todo.class.js'
 // import { TodoList } from './classes/todo.list.class';
-
-const todos = [];
-
 export const todoList = new TodoList();
-const tarea = new Todo('Aprender JavaScript');
-const tarea2 = new Todo('Comprar un unicornio');
 
-todoList.nuevoTodo(tarea);
-todoList.nuevoTodo(tarea2);
-console.log(todoList);
+todoList.todos.forEach(crearTodoHtml);
 
-crearTodoHtml(tarea);
